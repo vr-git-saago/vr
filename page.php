bc2ce5e87f907aacd8e1b6b8f83f5b535dc4c299
Jaanus Saago <jaanussaago@user.noreplay.github.com>
2021-03-05 17:27:23 +0200

    Esimese tunni teine pool

--- page.php
+++ page.php
@@ -7,7 +7,43 @@
 	$semesterduration = $semesterbegin->diff($semesterend);
 	$semesterdurationdays = $semesterduration->format("%r%a");
 	$semeserdurhtml = "\n <p>2021 kevadsemestri kestus on " .$semesterdurationdays ." päeva.</p> \n";
+	$today = new DateTime("now");
+	$fromsemesterbegin = $semesterbegin->diff($today);
+	$fromsemesterbegindays = $fromsemesterbegin->format("%r%a");
 	
+
+	if($fromsemesterbegindays <= $semesterdurationdays) {
+		$semesterprogress = "\n" .' <p>Semester edeneb: <meter min="0" max="' .$semesterdurationdays .'"value="' .$fromsemesterbegindays . '"></meter>.</p>' ."\n";
+	}
+	else {
+		$semesterprogress ="\n <p>Semester on lõppenud.</p> \n";
+	}
+
+	//loeme piltide kataloogi sisu
+	$picsdir = "../pics/";
+	$allfiles = array_slice(scandir($picsdir), 2);
+	//echo $allfiles[5];
+	//var_dump ($allfiles);
+	$allowedphototyes = ["image/jpeg", "image/png"];
+	$picfiles = [];
+
+	foreach($allfiles as $file) {
+		$fileinfo = getimagesize($picsdir .$file);
+		if(isset($fileinfo["mime"])) {
+			if(in_array($fileinfo["mime"], $allowedphototyes)) {
+				array_push($picfiles, $file);
+			}
+		}
+	}
+	$photocount = count($picfiles);
+	$photonum = mt_rand(0, $photocount-1);
+	$randomphoto = $picfiles[$photonum];
+
+	//for($x = 0; $x <10;$++) {
+		//tegevus
+	//}
+	
+
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -27,6 +63,8 @@
 		?>
 		<?php
 			echo $semeserdurhtml;
+			echo $semesterprogress;
 			?>
+	<img src="<?php echo $picsdir .$randomphoto; ?>" alt="vaade Haapsalus">		
 </body>
 </html>
