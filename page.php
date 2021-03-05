ca137bde84f302842406902d01dc76e0d3eb401e
Jaanus Saago <jaanussaago@user.noreplay.github.com>
2021-03-05 15:48:12 +0200

    Esimese tunni esimene pool

---
+++ page.php
@@ -0,0 +1,32 @@
+<?php 
+	$myname = "Jaanus Saago";
+	$currenttime = date("d.m.Y H:i:s");
+	$timehtml = "\n <p>Lehe avamise hetkel oli: " .$currenttime . ".</p> \n";
+	$semesterbegin = new DateTime("2021-1-25");
+	$semesterend = new DateTime("2021-6-30");
+	$semesterduration = $semesterbegin->diff($semesterend);
+	$semesterdurationdays = $semesterduration->format("%r%a");
+	$semeserdurhtml = "\n <p>2021 kevadsemestri kestus on " .$semesterdurationdays ." päeva.</p> \n";
+	
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+	<meta charset="utf-8">
+	<title>Veebirakendused ja nende loomine 2021</title>
+</head>
+<body>
+	<h1>
+	<?php
+		echo $myname;
+		?>
+	</h1>
+	<p>See leht on valminud õppetöö raames!</p>
+	<?php
+		echo $timehtml;
+		?>
+		<?php
+			echo $semeserdurhtml;
+			?>
+</body>
+</html>
